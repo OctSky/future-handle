883a51aee704649af47290292b6b89237bbee279
zhangjh <zhagnjh@fxiaoke.com>
2017-11-22 15:19:06 +0800

    zxing二维码test

--- my-future-handle/src/main/java/com/zjh/future/Main.java
+++ my-future-handle/src/main/java/com/zjh/future/Main.java
@@ -1,14 +1,20 @@
 package com.zjh.future;
 
-import java.util.Comparator;
+import com.zjh.future.utils.QRCodeUtil;
 
 /**
  * Created by zhangjh on 2017/11/21.
  */
 public class Main {
-    public static void main(String[] args) {
-        Comparator<Integer> comparator = (x, y) -> y.compareTo(x);
-        int value = comparator.compare(1, 2);
-        System.out.println(value);
+    public static void main(String[] args) throws Throwable {
+        //二维码编码
+        String content = "曾经，有一份真挚的爱摆在我面前，我却没有珍惜，等到失去的时候才后悔莫及。如果上天能再给我一次机会的话，我要对那个女孩子说三个字：" +
+                "\"我爱你\"！如果非要为这份爱加一个期限的话，我希望是一辈子！";
+        String filePath = "C:/Users/zhangjh/Desktop/test.png";
+        QRCodeUtil.encodeToFile(content, filePath, "png", 300, 300);
+
+        //二维码解码
+        String str = QRCodeUtil.decodeFromFile(filePath);
+        System.out.println("二维码解码：" + str);
     }
 }
